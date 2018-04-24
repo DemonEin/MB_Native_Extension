f7173649ffeb6322fe0053844531533126131a46
unknown <danielwarner00@gmail.com>
2018-04-23 19:14:52 -0700

    messing with git

---
+++ Module_system 1.171/module_info.py
@@ -0,0 +1,8 @@
+
+# Point export_dir to the folder you will be keeping your module
+# Make sure you use forward slashes (/) and NOT backward slashes (\)
+
+#note from DemonEin: don't track in git, or change upstream, this is for your workspace only (here you can see what I have)
+
+export_dir = "H:\Program Files (x86)\Steam\steamapps\common\MountBlade Warband\Modules\Mod 1"
+#export_dir = "C:/Program Files (x86)/Mount&Blade Warband/Modules/Native/"
