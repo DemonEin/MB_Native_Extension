f372d5590417680449df6408ca1a9474bfc5253d
unknown <danielwarner00@gmail.com>
2018-04-25 20:24:53 -0700

    finsihed dealing with module_info stuff

---
+++ module_info.py
@@ -0,0 +1,12 @@
+#From DemonEinstein: copy this file to the Module_system 1.171 folder and change export_dir before attempting to build
+#This is what I use, for example
+
+
+
+# Point export_dir to the folder you will be keeping your module
+# Make sure you use forward slashes (/) and NOT backward slashes (\)
+
+
+
+export_dir = "H:/Program Files (x86)/Steam/steamapps/common/MountBlade Warband/Modules/Mod 1/"
+#export_dir = "C:/Program Files (x86)/Mount&Blade Warband/Modules/Native/"
